dcaa6a22bce71ebe52db0d458a1540f0c60f4c25
Анастасия Алмазова <99196054+almazzzik11111@users.noreply.github.com>
2022-03-05 20:39:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int low = 0;  //  поместить сюда реализацию алгоритма
+    int high = size - 1;
+ 
+    int mid;
+ 
+    while (low <= high)
+      {
+        mid = (low + high) / 2;
+ 
+        if (value == arr[mid])
+        {
+            return mid;
+        }
+        else if (value > arr[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
   return 0; // если ничего не найдено
 }
